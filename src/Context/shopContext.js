9303afbb370a17b8cb261258b080e378cc0b3a87
Adarsh <adarsh9472351@gmail.com>
2023-07-21 18:02:03 +0530

    change faker image

--- src/Context/shopContext.js
+++ src/Context/shopContext.js
@@ -1,49 +1,43 @@
-import React, { useReducer } from 'react'
-import { createContext } from 'react'
-import { faker } from '@faker-js/faker';
-import { CartReducer } from './CartReducer'
-import { ProductReducer } from './ProductReducer';
+import React, { useReducer } from "react";
+import { createContext } from "react";
+import { faker } from "@faker-js/faker";
+import { CartReducer } from "./CartReducer";
+import { ProductReducer } from "./ProductReducer";
 
-export const ShopContext = createContext()
+export const ShopContext = createContext();
 
-faker.seed(99)
+faker.seed(99);
 
-const ShopState = ({children}) => {
-    
-    const products=[...Array(20)].map(()=>({
-        id:faker.datatype.uuid(),
-        name:faker.commerce.productName(),
-        price:faker.commerce.price(),
-        image:faker.image.image(),
-        inStock:faker.random.arrayElement([0,3,5,6,7]),
-        fastDelivery:faker.datatype.boolean(),
-        ratings:faker.random.arrayElement([1,2,3,4,5])
+const ShopState = ({ children }) => {
+  const products = [...Array(20)].map(() => ({
+    id: faker.datatype.uuid(),
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    image: faker.image.avatar(),
+    inStock: faker.random.arrayElement([0, 3, 5, 6, 7]),
+    fastDelivery: faker.datatype.boolean(),
+    ratings: faker.random.arrayElement([1, 2, 3, 4, 5]),
+  }));
 
-    }))
+  const [state, dispatch] = useReducer(CartReducer, {
+    products: products,
+    cart: [],
+  });
 
+  const [productState, productDispatch] = useReducer(ProductReducer, {
+    byRating: 0,
+    byOutOfStock: false,
+    byFastDelivery: false,
+    bySearch: "",
+  });
 
+  return (
+    <ShopContext.Provider
+      value={{ state, dispatch, productState, productDispatch }}
+    >
+      {children}
+    </ShopContext.Provider>
+  );
+};
 
-    const [state,dispatch]=useReducer(CartReducer,{
-        products:products,
-        cart:[]
-    })
-
-    const [productState,productDispatch]=useReducer(ProductReducer,{
-        byRating:0,
-        byOutOfStock:false,
-        byFastDelivery:false,
-        bySearch:'',
-        
-        
-    })
-
-    
-
-    return (
-        <ShopContext.Provider value={{state,dispatch,productState,productDispatch}}>
-            {children}
-        </ShopContext.Provider>
-    )
-}
-
-export default ShopState
+export default ShopState;
